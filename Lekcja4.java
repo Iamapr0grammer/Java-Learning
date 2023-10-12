6a0aea2288342032fae25b4cf876b6c6e56cda0f
Iamapr0grammer <127696002+Iamapr0grammer@users.noreply.github.com>
2023-10-12 13:52:02 +0200

    Operatory część 2

---
+++ Lekcja4.java
@@ -0,0 +1,19 @@
+public class Lekcja4 {
+    public static void main(String[] args) {
+
+        // operatory porównania
+        int a = 5, b = 6;
+        boolean logiczna = a == b;
+        System.out.println(logiczna);
+        // tak naprawde, operator sprawdza czy warunek jest prawdzwy, czy fałszywy
+
+        // && to oznacza and, czyli oba warunki muszą być spełnione
+        boolean logiczna2 = 5 >= 5 && 6>1;
+        System.out.println(logiczna2);
+
+        // podczas kiedy powdójna pionowa kresla || oznacza albo, jedno albo drugie. Logiczna alternatywa
+
+        // negacja, operator negacji to po prostu ! zamienia każdą możliwą wartość na ptrzeciwną.
+
+    }
+}
